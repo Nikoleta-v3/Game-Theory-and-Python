7508a1660cc915410aff39f5ef1abcaac81d5d71
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2017-02-16 12:25:34 +0000

    Initial write up of the workshop.
    The structre of the workshop have been discussed by me and Dr Knight.
    It will cover the following:
    
    1. An introduction to game theory and the IPD
    2. Using Axelrod
    3. Creating matches and tournaments
    4. Use the human strategy
    5. Write a strategy

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Game-Theory-and-Python
-Game Theory and Python
+
+This is a repository created to run a Game theory workshop at PyCon Namibia.
+
+Topics covered:
+
+1.Introduction to Game theory and the Iterated prisoner's dilemma (IPD)
+2. Creating matches and tournaments using Axelrod python library
+3. Playing against strategies of the IPD
+4. Writing strategies
+5. Contributing to Axelrod python library
---
+++ human.py
@@ -0,0 +1,36 @@
+"""This is a script which calls the human strategy and creates a match against
+any selected player from the Axelrod library.
+
+Usage:
+    human.py [-h] [-n NAME] [-t TURNS]
+
+ Options:
+      -h --help         Show This
+      -n NAME           The name of the human strategy [default: me]
+      -t TURNS          The number of turns [default: 5]
+"""
+
+import axelrod as axl
+from docopt import docopt
+import random
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    # get values
+    name = arguments['-n']
+    turns = int(arguments['-t'])
+
+    # create players and match
+    strategies = [s() for s in axl.strategies]
+    random = random.randint(0, 178)
+    opponent = strategies[random]
+    me = axl.Human(name=name)
+    players = [opponent, me]
+
+    # play the match and return winner and final score
+    match = axl.Match(players, turns=turns)
+    match.play()
+    print('You have competed against {}, the final score of the match is: '
+          '{} and the winner was {}'.format(opponent.name, match.final_score(),
+                                            match.winner()))
